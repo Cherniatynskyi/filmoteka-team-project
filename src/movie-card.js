b169f2a4d694938abb078e5fc10e6d19253fbd50
YuriiLeo <yuriileontenko@gmail.com>
2022-08-26 10:53:49 +0300

    add markup movie card

---
+++ src/movie-card.js
@@ -0,0 +1,17 @@
+// ul для карток, на най стилі, можливо треба буде перенести на інший тег
+        /* <ul class="gallery">
+
+</ul> */
+    
+// картка контейнер
+
+<div class="movie-card">
+    <a href="" class="movie-item">
+        <img class="movie-img" src="${}" alt="${}" />
+    </a>
+    <div class="movie-info">
+        <h3 class="movie-title">${ }</h3>
+        <p class="movie-genre">${ } | ${ }</p>
+    </div>
+</div>
+
