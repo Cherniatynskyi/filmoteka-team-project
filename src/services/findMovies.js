484b7bd3330ba9ce0db7fb19c2ec5d3482169d35
TetianaMartyniuk <martyniuk.tanja@gmail.com>
2022-08-27 15:33:57 +0300

    function find movie

--- src/services/findMovies.js
+++ src/services/findMovies.js
@@ -1,5 +1,5 @@
 const axios = require('axios').default;
-import { getMovies } from './getMovies';
+import { getMovies, API_URL, API_KEY } from './getMovies';
 
 
 const filmSearch = document.querySelector(".film-search");
@@ -12,8 +12,8 @@ function findMovies(e) {
     let request = e.target.firstElementChild.value;
     getMovies('search/movie', request, 1)
       .then((response) => {
-          console.log(response);
-            // clearGalleryList(response);
+          // console.log(response);
+            clearGalleryList(response);
             cardMarkup(response.data.results);
 
         })
@@ -29,27 +29,32 @@ export function clearGalleryList(response) {
 }
 
 function cardMarkup(array) {
-    console.log(array);
-    array.map((item) => {
+    // console.log(array);
+  const markup = array.map((item) => {
+    const dateMarkup = getYear(item.release_date);
         return `<li class="grid-movie-card">
       <a href="" class="movie-item">
         <img
           class="movie-img"
-          src="./img/Rectangle 5.jpg"
-          alt="GREYHOUND"
+          src="http://image.tmdb.org/t/p/w500${item.poster_path}"
+          alt="${item.title}"
           loading="lazy"
         />
         <div class="movie-info">
-          <h3 class="movie-title">GREYHOUND</h3>
+          <h3 class="movie-title">${item.original_title}</h3>
           <ul class="thumb">
             <li class="movie-genre">Drama, Action</li>
-            <li class="movie-date">| 2020</li>
-            <li class="movie-rating">10.0</li>
+            <li class="movie-date">| ${dateMarkup}</li>
+            <li class="movie-rating">${item.vote_average}</li>
           </ul>
         </div>
       </a>
     </li>`
-    }).join("")
+  }).join("")
+  moviesListContainer.insertAdjacentHTML("beforeend", markup)
 }
 
-
+function getYear(date) {
+  const dateArr = date.split("-");
+  return dateArr[0]
+}
