9a608bbb173f384e31bbdaf5c172d6ef57dcbfe6
TetianaMartyniuk <martyniuk.tanja@gmail.com>
2022-08-29 17:37:29 +0300

    function that will get movie by id

---
+++ src/services/MovieObjectByID.js
@@ -0,0 +1,20 @@
+import { getMovies } from "./getMovies";
+
+export async function getMovieByID(id) {
+    const nul = null;
+    try {
+        const movieObj = await getMovies(`/movie/${id}`, nul, 1)
+        console.log(movieObj.data); //прилітає об'єкт з детальною інформацією
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//!!!!!!! функція не підключена до index.html
+// console.log(getMovieByID(990691));
+
+
+//id-шники для прикладу
+// 990691
+// 718789
+// 453395
--- src/services/addFindMovieInStorage.js
+++ src/services/addFindMovieInStorage.js
@@ -4,13 +4,3 @@ export function addMoviesInStorage(moviesArray) {
     localStorage.setItem(KEY, JSON.stringify(moviesArray))
 }
 
-
-// export function getMovieFromStorageByID(id) {
-//     const getData = JSON.parse(localStorage.getItem(KEY));
-//     getData.map((item) => {
-//         if (item.id === id) {
-//             console.log(id);
-//         }
-//     })
-    
-// }
