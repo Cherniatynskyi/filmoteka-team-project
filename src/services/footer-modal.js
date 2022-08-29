78a92f791453d7bc9c780c329e3d7e92ccb0a361
Mark Cherniatynskyi <markushalot34@gmail.com>
2022-08-29 18:41:07 +0300

    fixed modal windows

--- src/services/footer-modal.js
+++ src/services/footer-modal.js
@@ -4,13 +4,35 @@
       openModalBtn: document.querySelector('[data-modal-open]'),
       closeModalBtn: document.querySelector('[data-modal-close]'),
       modal: document.querySelector('[data-modal]'),
+      backdrop: document.querySelector('[backdrop]')
     };
   
-    refs.openModalBtn.addEventListener('click', toggleModal);
-    refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdrop.addEventListener('click', onBackDropClick);
+
   
-    function toggleModal() {
-        refs.modal.classList.toggle('is-hidden');
-        document.body.classList.toggle('no-scroll')
+  function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+      var htmlEl = document.getElementsByTagName("HTML")[0]
+      htmlEl.classList.toggle('no-scroll');
+  }
+
+  function onBackDropClick(evt) {
+    if (!evt.target.classList.contains('backdrop')) {
+      return
     }
-  })();
+    refs.modal.classList.add('is-hidden');
+    var htmlEl = document.getElementsByTagName("HTML")[0]
+    htmlEl.classList.remove('no-scroll');
+  }
+
+  function onCloseTeamModal() {
+    refs.modal.classList.add('is-hidden');
+    var htmlEl = document.getElementsByTagName("HTML")[0]
+    htmlEl.classList.remove('no-scroll');
+  }
+
+})();
+  
+
--- src/services/modalTemplate.js
+++ src/services/modalTemplate.js
@@ -1,59 +1,19 @@
-// const axios = require('axios').default;
-
-// const cardContEl = document.querySelector('.js-card__cont');
-// console.dir(cardContEl);
-// const movieGrid = document.querySelector('.movie-grid-list');
-// const gridMovieCard = document.querySelector('.grid-movie-card');
-
-// movieGrid.addEventListener('click', onCardClik());
-
-// cardContEl.addEventListener('click', onCardConteinerClik);
-
-// function onCardClik(event) {
-//   const currentFilms = [];
-//   if (event.currentFilms === movieGrid.childNodes) {
-//     onOpenModal();
-//   }
-// }
-// console.dir(movieGrid.childNodes);
-// function onOpenModal() {
-//   document.body.classList.add('hidden');
-// }
-
-// movieCardEl.addEventListener('click', () => {
-//   console.log(cardContEl.classList);
-//   cardContEl.classList.add('hidden');
-// });
-
-// function onCardConteinerClik(event) {
-//   const isCard = event.movieGrid.childNodes.contains('.movie-grid-list');
-//   if (!isCard) {
-//     return;
-//   }
-//   const currentActiveCard = document.querySelector('.card__cont');
-//   if (currentActiveCard) {
-//     currentActiveCard.classList.remove('hidden');
-//   }
-//   const swatchEl = event.movieGrid.childNodes;
-//   const parentCard = swatchEl.closest('.card__cont');
-
-//   parentCard.classList.add('hidden');
-// }
 import { getMovieByID } from "./MovieObjectByID";
 
 const openModalCard = document.querySelector('[data-modalCard-open]');
 const closeModalCard = document.querySelector('[data-modalCard-close]');
 const modalCardCont = document.querySelector('[data-modalCard]');
+const backdrop = document.querySelector('[backdrop]')
 
 openModalCard.addEventListener('click', onOpenModalCard);
 closeModalCard.addEventListener('click', onCloseModalCard);
+modalCardCont.addEventListener('click', onBackModalDropClick)
+window.addEventListener('keydown', onEscClick)
 
 function onOpenModalCard(event) {
-  // debugger
-  // console.log(event.target.attributes.id.value);
   
   if (event.target.classList.contains('grid-movie-card')) {
-    //записуємо id в змінну
+
     const filmID = event.target.attributes.id.value;
     //визиваємо ф-ію, що записує об'єкт фільму у змінну
     const filmObject = getMovieByID(filmID);
@@ -61,51 +21,31 @@ function onOpenModalCard(event) {
 
 
     modalCardCont.classList.remove('no-activ');
-    document.body.classList.add('no-scroll');
+     var htmlEl = document.getElementsByTagName("HTML")[0]
+     htmlEl.classList.add('no-scroll');
 
   } 
   return
-    // modalCardCont.classList.remove('no-activ');
-    // document.body.classList.add('no-scroll');
-  // modalCardCont.classList.remove('no-activ');
-  // document.body.classList.add('no-scroll');
 }
 
 function onCloseModalCard(event) {
   modalCardCont.classList.add('no-activ');
-  document.body.classList.remove('no-scroll');
+  var htmlEl = document.getElementsByTagName("HTML")[0]
+  htmlEl.classList.remove('no-scroll');
 }
-// один з не працюючих варіантів достукатись до лішки
-// let list = document.querySelectorAll('.movie-grid-list li a');
-// console.log(list);
-// list.forEach(item => {
-//   item.addEventListener('click', e => {
-//     list.forEach(el => {
-//       el.classList.remove('is-hidden');
-//     });
-//     item.classList.add('is-hidden');
-//   });
-// });
 
-// // =============================
-// // закриття по esc
-// // function oneItemCardClick(event) {
-// //   event.preventDefault();
-// //   if (!event.target.classList.contains('movie-card')) {
-// //     return;
-// //   }
-// //   const instance = basicLightbox.create(
-// //     `<img src="${evt.target.dataset.source}">`
-// //   );
-// //   instance.show(() => window.addEventListener('keydown', oneItemKeyPress));
-// //   function oneItemKeyPress(event) {
-// //     if (event.key === 'Escape') {
-// //       instance.close(() =>
-// //         window.removeEventListener('keydown', oneItemKeyPress)
-// //       );
-// //       console.log(event.key);
-// //       return;
-// //     }
-// //     return;
-// //   }
-// // }
+function onBackModalDropClick(evt) {
+    if (!evt.target.classList.contains('card__cont')) {
+      return
+    }
+    modalCardCont.classList.add('no-activ');
+    var htmlEl = document.getElementsByTagName("HTML")[0]
+    htmlEl.classList.remove('no-scroll');
+}
+  
+function onEscClick(evt) {
+  if (evt.key === 'Escape') {
+    onCloseModalCard()
+    backdrop.classList.add('is-hidden')
+  }
+}
