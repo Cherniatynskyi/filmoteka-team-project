8492dab6e7461eea003390df8562651da5a1fa45
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-08-30 14:50:44 +0300

    make listener on button in modal, and response the filmobject to mylibrary

--- src/services/modalTemplate.js
+++ src/services/modalTemplate.js
@@ -1,15 +1,28 @@
 import { getMovieByID } from './MovieObjectByID';
+import { addWatchedMoviesInStorage, addQueueMoviesInStorage} from './myLibraryPage';
 
 const openModalCard = document.querySelector('[data-modalCard-open]');
 const closeModalCard = document.querySelector('[data-modalCard-close]');
 const modalCardCont = document.querySelector('[data-modalCard]');
 const backdrop = document.querySelector('[backdrop]');
 const cardContMarking = document.querySelector('.card__cont-marking');
+const cardTableContainer = document.querySelector('.card__table-container');
+const addToWatchedButton = document.querySelector('[data-add-to-watched]');
+const addToQueueButton = document.querySelector('[data-add-to-queue]');
+
+
+
+
+
 
 openModalCard.addEventListener('click', onOpenModalCard);
 closeModalCard.addEventListener('click', onCloseModalCard);
 modalCardCont.addEventListener('click', onBackModalDropClick);
 window.addEventListener('keydown', onEscClick);
+
+
+
+
 
 function onCloseModalCard() {
   modalCardCont.classList.add('no-activ');
@@ -37,19 +50,21 @@ function onOpenModalCard(event) {
   if (event.target.classList.contains('grid-movie-card')) {
     const filmID = event.target.attributes.id.value;
     //визиваємо ф-ію, що записує об'єкт фільму у змінну
-    const filmObject = getMovieByID(filmID).then(res => cardMarkUp(res));
-    console.log(filmObject);
+    getMovieByID(filmID).then(res => cardMarkUp(res));
 
     modalCardCont.classList.remove('no-activ');
     var htmlEl = document.getElementsByTagName('HTML')[0];
     htmlEl.classList.add('no-scroll');
+
   }
   return;
 }
+
+
 function cardMarkUp(filmObject) {
-  const markUP = `<img class="card__img" src="http://image.tmdb.org/t/p/w500${filmObject.poster_path}" alt="${filmObject.title}" />
-    <div class="card__table-container">
-      <h1 class="card__table-heder">${filmObject.title}</h1>
+  const markUPImg = `<img class="card__img" src="http://image.tmdb.org/t/p/w500${filmObject.poster_path}" alt="${filmObject.title}" />`;
+
+  const markUp = `<h1 class="card__table-heder">${filmObject.title}</h1>
       <table class="card__table">
         <tr class="card__table-vote">
           <td class="card__table-name ">Vote / Votes</td>
@@ -71,11 +86,12 @@ function cardMarkUp(filmObject) {
       <div class="card__about">
         <h2 class="card__about-heder">About</h2>
         <p class="card__about-text">${filmObject.overview}</p>
-      </div>
-      <div class="card__button">
-        <button class="card__button-text">add to Watched</button>
-        <button class="card__button-text">add to queue</button>
-      </div>
-    </div>`;
-  cardContMarking.insertAdjacentHTML('beforeend', markUP);
+      </div>`;
+  cardTableContainer.insertAdjacentHTML('afterbegin', markUp);
+  cardContMarking.insertAdjacentHTML('afterbegin', markUPImg);
+  addToWatchedButton.addEventListener('click', () => addWatchedMoviesInStorage(filmObject));
+  addToQueueButton.addEventListener('click', () => addQueueMoviesInStorage(filmObject));
+
 }
+
+
