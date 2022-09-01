e64ab3c02b6d8d826f13ca635895f91d5c1db09a
Palamarchuk_Tanya <t0973753805@gmail.com>
2022-09-01 23:19:40 +0300

    bag_modalTemp
    підправила скрол модалки

--- src/services/modalTemplate.js
+++ src/services/modalTemplate.js
@@ -48,8 +48,8 @@ function onOpenModalCard(event) {
   if (event.target.classList.contains('grid-movie-card')) {
     const filmID = event.target.attributes.id.value;
 
-    addToQueueButton.classList.remove('card-buton-change')
-    addToWatchedButton.classList.remove('card-buton-change')
+    addToQueueButton.classList.remove('card-buton-change');
+    addToWatchedButton.classList.remove('card-buton-change');
 
     getMovieByID(filmID).then(res => cardMarkUp(res));
 
@@ -70,6 +70,8 @@ function cardMarkUp(filmObject) {
 
   let markupGenre = filmObject.genres;
   let cardGenre = markupGenre.map(genr => genr.name);
+
+  cardGenre: markupGenre.length > 0 ? cardGenre.join(', ') : 'Unknown';
 
   const markUp = `<h1 class="card__table-heder">${filmObject.title}</h1>
       <table class="card__table">
@@ -104,7 +106,9 @@ function addWatchedMoviesInStorage(addWatchedMovie) {
     return;
   }
   let watchedMovies = JSON.parse(watchedMovieInStorage);
-  const watchedMoviesInStorageArr = watchedMovies.find(item => item.id === addWatchedMovie.id);
+  const watchedMoviesInStorageArr = watchedMovies.find(
+    item => item.id === addWatchedMovie.id
+  );
   if (watchedMoviesInStorageArr) {
     localStorage.setItem(KEY_WATCHED, JSON.stringify(watchedMovies));
   } else {
@@ -120,7 +124,9 @@ function addQueueMoviesInStorage(addQueueMovie) {
     return;
   }
   let queueMovies = JSON.parse(queueMovieInStorage);
-  const queueMoviesInStorageArr = queueMovies.find(item => item.id === addQueueMovie.id);
+  const queueMoviesInStorageArr = queueMovies.find(
+    item => item.id === addQueueMovie.id
+  );
   if (queueMoviesInStorageArr) {
     localStorage.setItem(KEY_QUEUE, JSON.stringify(queueMovies));
   } else {
@@ -192,28 +198,26 @@ function removeMovieFromQueue(movie) {
   getQueueMoviesInStorage();
 }
 
-
-// Dynamic changing text-content on modal buttons 
+// Dynamic changing text-content on modal buttons
 
 addToQueueButton.addEventListener('click', () => {
-  if (addToQueueButton.textContent == "Remove from queue") {
-    addToQueueButton.textContent = "Removed from Queue"
-    addToQueueButton.classList.add('card-buton-change')
+  if (addToQueueButton.textContent == 'Remove from queue') {
+    addToQueueButton.textContent = 'Removed from Queue';
+    addToQueueButton.classList.add('card-buton-change');
   }
-  if (addToQueueButton.textContent == "Add to queue") {
-    addToQueueButton.textContent = "Added to Queue"
-    addToQueueButton.classList.add('card-buton-change')
+  if (addToQueueButton.textContent == 'Add to queue') {
+    addToQueueButton.textContent = 'Added to Queue';
+    addToQueueButton.classList.add('card-buton-change');
   }
 });
 
 addToWatchedButton.addEventListener('click', () => {
-  if (addToWatchedButton.textContent == "Remove from watched") {
-    addToWatchedButton.textContent = "Removed from watched"
-    addToWatchedButton.classList.add('card-buton-change')
-    
+  if (addToWatchedButton.textContent == 'Remove from watched') {
+    addToWatchedButton.textContent = 'Removed from watched';
+    addToWatchedButton.classList.add('card-buton-change');
   }
-  if (addToWatchedButton.textContent == "Add to watched") {
-    addToWatchedButton.textContent = "Added to Watched"
-    addToWatchedButton.classList.add('card-buton-change')
+  if (addToWatchedButton.textContent == 'Add to watched') {
+    addToWatchedButton.textContent = 'Added to Watched';
+    addToWatchedButton.classList.add('card-buton-change');
   }
 });
